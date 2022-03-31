b0d21e541cc35325b4208e46c14fe1fb80cb078b
inv.bbarros <inv.bbarros@ifood3rd.com>
2022-03-31 15:16:11 -0300

    metodos para filtrar os campos de usuario

--- api/src/model/usuario.go
+++ api/src/model/usuario.go
@@ -1,6 +1,10 @@
 package model
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Usuario - modelo de um usuário
 type Usuario struct {
@@ -10,4 +14,37 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"criadoEm,omitempty"`
+}
+
+// validaUsuario : método para validar se os campos estão vazios
+func (usuario *Usuario) validaUsuario() error {
+	if usuario.Nome == "" {
+		return errors.New("Nome é obrigatório!")
+	}
+	if usuario.Nick == "" {
+		return errors.New("Nick é obrigatório!")
+	}
+	if usuario.Email == "" {
+		return errors.New("Email é obrigatório!")
+	}
+	if usuario.Senha == "" {
+		return errors.New("Senha é obrigatório!")
+	}
+	return nil
+}
+
+// formatar : formata strings para tirar espacos do inicio e do fim
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
+}
+
+// Preparar : chama os métodos para validar e formatar os dados recebidos do usuario
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validaUsuario(); erro != nil {
+		return erro
+	}
+	usuario.formatar()
+	return nil
 }
